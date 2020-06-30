5242828d0ccdd0f8b23fb835cc74f6f258855180
Sven Gehring <cbrxde@gmail.com>
2020-06-30 11:06:13 +0200

    restructure script files

---
+++ DB-Scripts/DatabaseSeed.sql
@@ -0,0 +1,10 @@
+-- drop database
+
+-- Datenbank löschen, wenn diese Bereits existiert. 
+
+-- erstellt am 11.06.2020
+-- Jennifer Mentner & Sven Gehring
+
+-- geändert am xx.xx.xxxx
+-------------------------------------------------
+
