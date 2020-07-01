0010aa8a355111c989702531818469f7bd8e6147
JenniferSusann <50921997+JenniferSusann@users.noreply.github.com>
2020-07-01 07:52:57 +0200

    Strasse & Anrede test

--- TI4-DT-SJ/DatabaseTests.cs
+++ TI4-DT-SJ/DatabaseTests.cs
@@ -24,5 +24,37 @@ namespace TI4_DT_SJ
 
       return success;
     }
+    public static bool teststrasse()
+    {
+      bool success = true;
+      Database.Instance.runCommand("INSERT INTO strasse (id, ort_id, strassenname, hausnummer) VALUES(1, 1, 'Seestrasse', 241)").ExecuteNonQuery();
+      SQLDataReader reader = Database.Instance.getCommand("SELECT * FROM strasse LEFT JOIN ort ON strasse.ort_id=ort.id").ExecuteReader();
+      while (reader.Read())
+      {
+        if (reader.GetInt32(0) != 1) success = false;
+        if (reader.GetInt32(1) != 1) success = false;
+        if (reader.GetVarchar255(2) != "Seestrasse") success = false;
+        if (reader.GetInt32(3) != 241) success = false;
+      }
+      reader.Close();
+
+      return success;
+    }
+
+    public static bool testanrede()
+    {
+      bool success = true;
+      Database.Instance.runCommand("INSERT INTO anrede (id, bezeichnung) VALUES(1, Herr)").ExecuteNonQuery();
+      SQLDataReader reader = Database.Instance.getCommand("SELECT * FROM anrede").ExecuteReader();
+      while (reader.Read())
+      {
+        if (reader.GetInt32(0) !=1) success = false;
+        if (reader.GetVarchar20(1) != "Herr") success = false,
+      }
+      reader.close();
+
+      return success;
+    }
+
   }
 }
