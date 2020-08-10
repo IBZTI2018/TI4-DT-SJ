e5b5b816f11bf891376656ad142805061bf54452
JenniferSusann <50921997+JenniferSusann@users.noreply.github.com>
2020-08-10 12:23:35 +0200

    bin es bizeli verwirrt gsi

--- DB-Scripts/DatabaseSeed.sql
+++ DB-Scripts/DatabaseSeed.sql
@@ -32,51 +32,8 @@ INSERT INTO person (anrede_id, adresse_id, vorname, nachname, geburtsdatum, emai
 INSERT INTO anbieter (person_id, aufnahmedatum, prov_aufnahmedatum, bonitaetspruefung, unterschrift)
     VALUES  ((SELECT id FROM person WHERE vorname = 'Jennifer'), '20200520', '20200201', 1, 1);
 
-insert into nachfrager (id, person_id)
+INSERT INTO nachfrager (id, person_id)
     VALUES  (1, (SELECT id FROM person WHERE vorname = 'Fritz'));
-GO
-
-insert into qualitaetspruefer (id, person_id, lohn)
-    VALUES  (1, (SELECT id FROM person WHERE vorname = 'Sven'), 5000);
-GO
-
-insert into bewertung (id, anbieter_id, nachfrager_id, bezeichnung, score)
-    VALUES  (1, (SELECT id FROM anbieter WHERE person_id = '2'), (SELECT id FROM nachfrager WHERE person_id = '1'), 'preis ok, aussehen sehr gut', 5);
-GO
-
-insert into aboart (id, bezeichnung, gebuehr)
-    VALUES  (1, 'small', 400),
-            (2, 'big', 800),
-            (3, 'luxussmall', 1000);
-GO
-
-insert into abo (id, anbieter_id, aboart_id)
-    VALUES  (1, (SELECT id FROM anbieter WHERE person_id = '2'), (SELECT id FROM aboart WHERE bezeichnung = 'big'));
-GO
-
-insert into standort (id, bezeichnung)
-    VALUES  (1, 'Zuerich'),
-            (2, 'Aarau'),
-            (3, 'Bern'),
-            (4, 'Basel');
-GO
-
-insert into standplatz (id, standort_id, standplatz_nr)
-    VALUES  (1, (SELECT id FROM standort WHERE bezeichnung = 'Zuerich'), 20),
-            (2, (SELECT id FROM standort WHERE bezeichnung = 'Aarau'), 10);
-GO
-
-insert into termin (id, standplatz_id, anbieter_id, datum)
-    VALUES  (1, (SELECT id FROM standplatz WHERE standort_id = '1'), (SELECT id FROM anbieter WHERE person_id = '2'), '22.08.2020');
-GO
-
-insert into rechnung (id, abo_id, anbieter_id, termin_id, rechnungs_nr, betrag)
-    VALUES  (1, (SELECT id FROM abo WHERE aboart_id = '2'), (SELECT id FROM anbieter WHERE person_id = '2'), (SELECT id FROM termin WHERE datum = '22.08.2020'), 123456789, 420);
-GO
-
-insert into qualitaetsbewertung (id, anbieter_id, qualitaetspruefer_id, bezeichnung)
-    VALUES (1, (SELECT id FROM anbieter WHERE person_id = '2'), (SELECT id FROM qualitaetspruefer WHERE person_id = '3'), 'prüfungen bestanden');
-GO
 
 INSERT INTO qualitaetspruefer (person_id, lohn)
     VALUES  ((SELECT id FROM person WHERE vorname = 'Jennifer'), 5000);
